27ac6828d4c6ef4989b1ff535d6f9d97e921bc3c
Super User <maestronew20@gmail.com>
2024-07-22 16:11:33 +0300

    task 13 lab 2

--- kiselev.maxim/I1/task13_Lab2.cpp
+++ kiselev.maxim/I1/task13_Lab2.cpp
@@ -1,30 +1,39 @@
- #include <iostream>
+#include <iostream>
 
- int findmax(int nums[], int size) {
-    if (size == 0) return 0; 
+int findmax(int nums[], int size)
+{
+    if (size == 0)
+        return 0;
 
-    int max = 1; 
-    int current = 1; 
+    int max = 1;
+    int current = 1;
 
-  
-    for (int i = 1; i < size; ++i) {
-        if (nums[i] < nums[i - 1]) { 
-            ++current; 
-        } else { 
-            if (current > max) { 
+
+    for (int i = 1; i < size; ++i)
+    {
+        if (nums[i] < nums[i - 1])
+        {
+            ++current;
+        }
+        else
+        {
+            if (current > max)
+            {
             }
-            current = 1; 
+            current = 1;
         }
     }
 
-    if (current > max) {
+    if (current > max)
+    {
         max = current;
     }
 
-    return max; 
- }
+    return max;
+}
 
- int main() {
+int main()
+{
     int size;
 
     std::cout << "Введите размер массива: ";
@@ -33,13 +42,13 @@
     int nums[size];
 
     std::cout << "Введите элементы массива: ";
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i)
+    {
         std::cin >> nums[i];
     }
 
-    int max= findmax(nums, size);
-    std::cout << "Наибольшая длина монотонно убывающего фрагмента: " 
-              << max<< std::endl;
+    int max = findmax(nums, size);
+    std::cout << "Наибольшая длина монотонно убывающего фрагмента: " << max << std::endl;
 
     return 0;
- }
+}
