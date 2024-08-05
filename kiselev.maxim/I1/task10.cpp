8ec645c8a2f46f75fcd6d50844a7a850720da9e5
Super User <maestronew20@gmail.com>
2024-08-05 19:27:01 +0300

    lab1 try3

--- kiselev.maxim/I1/task10.cpp
+++ kiselev.maxim/I1/task10.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 int main()
 {
-int First;
-std::cin >> First;
-int Second, Third, Number;
-Number = 0;
-Third = Second;
-Second = First;
-  while (First != 0)
+    int First;
+    std::cin >> First;
+    int Second, Third, Number;
+    Number = 0;
+    Third = Second;
+    Second = First;
+    while (First != 0)
     {
-       if (Second < Third && First < Second)
+        if (Second < Third && First < Second)
         {
-           Number += 1;
+            Number += 1;
         }
-      Third = Second;
-      Second = First;
-      std::cin >> First;
+        Third = Second;
+        Second = First;
+        std::cin >> First;
     }
-std::cout << Number;
-return 0;
+    std::cout << Number;
+    return 0;
 }
-
-
--- kiselev.maxim/I1/task11.cpp
+++ kiselev.maxim/I1/task11.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 int main()
 {
-  int First;
-  std::cin >> First;
-  int Second, Number;
-  Number = -1;
+    int First;
+    std::cin >> First;
+    int Second, Number;
+    Number = -1;
     while (First != 0)
     {
-      if (First % Second == 0)
+        if (First % Second == 0)
         {
-          Number += 1;
+            Number += 1;
         }
-      Second = First;
-      std::cin >> First;
+        Second = First;
+        std::cin >> First;
     }
-  std::cout << Number;
-return 0;
+    std::cout << Number;
+    return 0;
 }
-
